fa31d4f0c9d1ee1fcd50341bd49be7a5f826eb7c
Khuna Chuan <chuan_8297@hotmail.com>
2018-03-29 14:02:30 +0700

    add user create question story

--- functional_test.py
+++ functional_test.py
@@ -23,7 +23,23 @@ class HomePageTest(unittest.TestCase):
         # She see the browser title
         self.assertIn('Quiz', self.browser.title)
 
+        # she see the web heading
+        heading1 = self.browser.find_element_by_tag_name('h1')
+        self.assertIn('create question', heading1.text)
+
         # She see the input box to create question
+        # two radio button to select answer
+        # and create button
+        question_text = self.browser.find_element_by_id('question')
+        self.assertEqual(qustion_text.getattribute('placeholder', 'Enter Question'))
+
+        ans_true = self.browser.find_element_by_id('ans_t')
+        self.assertEqual(qustion_text.getattribute('value', 'True'))
+
+        ans_false = self.browser.find_element_by_id('ans_f')
+        self.assertEqual(qustion_text.getattribute('value', 'False'))
+
+
 
         self.fail('Finished the test!')
 
